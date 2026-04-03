3f7f79af4ad3969741b94fa44552b0b6638ea23a
wangzhilin777 <993517606@qq.com>
2026-04-04 05:13:14 +0800

    继续收紧FTP/SFTP/SMB首次预览首帧揭幕

--- lib/widgets/common/thumbnail/decorated.dart
+++ lib/widgets/common/thumbnail/decorated.dart
@@ -825,12 +825,9 @@ class _AutoPlayVideoThumbnailState extends State<_AutoPlayVideoThumbnail> {
                 isChunkedRemotePreview &&
                 errorCooldownStartedAt != null &&
                 DateTime.now().millisecondsSinceEpoch - errorCooldownStartedAt < 4000;
-            final ftpForceVisible =
-                isCurrent && isFtpPreview && (keepLastFrameVisible || hasStablePreviewFrame || (_videoSurfaceVisible && inChunkedErrorCooldown));
-            final sftpForceVisible =
-                isCurrent && isSftpPreview && (keepLastFrameVisible || hasStablePreviewFrame || (_videoSurfaceVisible && inChunkedErrorCooldown));
-            final smbForceVisible =
-                isCurrent && isSmbPreview && (keepLastFrameVisible || hasStablePreviewFrame || (_videoSurfaceVisible && inChunkedErrorCooldown));
+            final ftpForceVisible = isCurrent && isFtpPreview && _videoSurfaceVisible;
+            final sftpForceVisible = isCurrent && isSftpPreview && _videoSurfaceVisible;
+            final smbForceVisible = isCurrent && isSmbPreview && _videoSurfaceVisible;
             final webdavForceVisible =
                 isCurrent && isWebdavPreview && _playRequestedForCurrentFocus && controller.status != VideoStatus.error;
             final show = _videoSurfaceVisible ||
